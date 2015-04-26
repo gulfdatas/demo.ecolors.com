26a1d0f68d399d01b96140c46ce2533fd47528ba
gulfdatas <agalal@gulfdatas.com>
2015-04-26 12:31:39 +0200

    home slider

---
+++ pages/home.php
@@ -0,0 +1,38 @@
+	<div class="tp-banner-container">
+		<div class="tp-banner" >
+			<ul>
+				<!-- SLIDE  -->
+				<li data-transition="fade" data-slotamount="7" data-masterspeed="1500" >
+					<!-- MAIN IMAGE -->
+					<img src="../assets/images/slider/slider1.png"  alt="slidebg1"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+					<!-- LAYERS -->
+					<!-- LAYER NR. 1 -->
+					<div class="tp-caption lightgrey_divider skewfromrightshort fadeout"
+						data-x="85"
+						data-y="224"
+						data-speed="500"
+						data-start="1200"
+						data-easing="Power4.easeOut">
+					</div>
+					...
+
+				</li>
+				<!-- SLIDE  -->
+				<li data-transition="zoomout" data-slotamount="7" data-masterspeed="1000" >
+					<!-- MAIN IMAGE -->
+					<img src="../assets/images/slider/slider2.png"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+					<!-- LAYERS -->
+					<!-- LAYER NR. 1 -->
+					<div class="tp-caption lightgrey_divider skewfromrightshort fadeout"
+						data-x="85"
+						data-y="224"
+						data-speed="500"
+						data-start="1200"
+						data-easing="Power4.easeOut">
+					</div>
+					...
+				</li>
+				....
+			</ul>
+		</div>
+	</div>
